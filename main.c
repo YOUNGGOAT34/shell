9b7e345485a13d0ace9f1f3d80d63b7930c8ef43
GOAT <lucasonyiego283@gmail.com>
2026-01-10 12:14:21 +0300

    parse invalid commands

--- main.c
+++ main.c
@@ -1,11 +1,22 @@
 
 #include <stdio.h>
+#include <string.h>
+
 
 typedef unsigned int u32;
 typedef int i32;
 typedef char i8;
 
 int main(){
-   printf("Hello world\n");
+   setbuf(stdout,NULL);
+   printf("$ ");
+
+   i8 command[100];
+   fgets(command,100,stdin);
+
+   command[strlen(command)-1]='\0';
+
+   printf("%s: command not found\n",command);
+
    return 0;
 }
